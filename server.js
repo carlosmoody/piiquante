bd52a67832efa56a53b6bd8d9fb7dd80d919d770
carlosmoody <otralongo@icloud.com>
2023-04-12 08:45:33 +0200

    server creation

---
+++ server.js
@@ -0,0 +1,17 @@
+const dotenv = require("dotenv");
+dotenv.config;
+
+const app = require("./app");
+
+const PORT = process.env.PORT;
+console.log(PORT);
+
+const http = require("http");
+
+const server = http.createServer(app);
+
+server.on("listening", () => {
+  console.log("Listening on port " + PORT);
+});
+
+server.listen(PORT);
