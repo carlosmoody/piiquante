7262987401ec6d46ad57b12a88692765ea15bf21
carlosmoody <otralongo@icloud.com>
2023-04-12 10:29:18 +0200

    signup + login creation

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,44 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.signup = (req, res, next) => {
-
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() =>
+          res.status(201).json({ message: "Utilisateur créé avec succès" })
+        )
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
 
 exports.login = (req, res, next) => {
-
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ message: "Erreur d'authentification" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res
+              .status(401)
+              .json({ message: "Erreur d'authentification" });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: "TOKEN",
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
