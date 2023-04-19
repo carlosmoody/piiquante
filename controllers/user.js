3932526513dfc8d84ce7879e64b88243185ac4b3
carlosmoody <otralongo@icloud.com>
2023-04-19 15:58:29 +0200

    ajout de la validation de l'email

--- controllers/user.js
+++ controllers/user.js
@@ -1,25 +1,34 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const validator = require('validator');
 
 const TOKEN_SECRET = process.env.TOKEN_SECRET;
 
 exports.signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
+  const email = req.body.email;
+  const password = req.body.password;
+  const saltRounds = 10;
+
+  if (!validator.isEmail(email)) {
+    res.status(400).json({ message: "Erreur de saisie de l'e-mail" });
+  } else {
+    bcrypt
+    .hash(password, saltRounds)
     .then((hash) => {
       const user = new User({
         email: req.body.email,
         password: hash,
       });
       user
-        .save()
-        .then(() =>
-          res.status(201).json({ message: "Utilisateur créé avec succès" })
-        )
-        .catch((error) => res.status(400).json({ error }));
+      .save()
+      .then(() =>
+      res.status(201).json({ message: "Utilisateur créé avec succès" })
+      )
+      .catch((error) => res.status(400).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
+  }
 };
 
 exports.login = (req, res, next) => {
